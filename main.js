8195c444e1b87e9553a22d378c9a1e42f1d16cf3
Keviweto-Yhoshu <89389070+Keviweto-Yhoshu@users.noreply.github.com>
2021-10-21 15:58:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function Home() {
+    window.locaton = "index.html" ;
+}
+
+
+function GridView() {
+    window.location = "Grid.html" ;
+}
+
+ function slideView() {
+     window.location = "slide.html" ;
+ }
